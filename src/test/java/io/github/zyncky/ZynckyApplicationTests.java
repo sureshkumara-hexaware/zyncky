c0d03d1e9078a29f4332acee318175e45bf1e67c
Suresh Kumar A <sureshk11@hexaware.com>
2022-11-02 22:14:22 +0530

    first commit

---
+++ src/test/java/io/github/zyncky/ZynckyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zyncky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZynckyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
